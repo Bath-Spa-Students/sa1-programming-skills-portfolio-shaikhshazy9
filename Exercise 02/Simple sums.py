bd56fa06a025b5fcb43e33420eec41e51506c2ad
shaikhshazy9 <shaikhshazy789@gmail.com>
2024-10-22 13:28:38 +0400

    Exercise 1
    Coding is cool

---
+++ Exercise 02/Simple sums.py
@@ -0,0 +1,20 @@
+"""Exercise 2: Simple Sums - 15 Marks
+
+In this exercise, you will create and work with integer variables, perform arith-
+metic operations, and print the result to the console.
+Steps:
+1. Declare a variable and initialize it with the integer value 8.
+2. Declare a second variable and initialize it with the integer value 10.
+3. Declare a third variable that stores the sum of first two numbers.
+4. Print the value of the sum to the console."""
+#Step 1 : Create a variable with the number 8.
+first_number = 8 
+
+#Step 2 : Create another variable with the number 10.
+second_number = 10 
+
+#Step 3 : Add both of the numbers and save the result.
+sum_of_numbers = first_number + second_number 
+
+#Step 4: Show the result on the screen
+print("The sum is:", sum_of_numbers) #print the sum (8+10)
