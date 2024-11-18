3b80b925017d237bf09535b9f32e5acba52157d2
shaikhshazy9 <shaikhshazy789@gmail.com>
2024-11-19 00:24:28 +0400

    Variables

---
+++ Bonus exercises/Variables.py
@@ -0,0 +1,9 @@
+"""Q- Assign a message to a variable, and print that message. Then change the value of the
+variable to a new message, and print the new message."""
+#give the first message to the variable 
+message = "Hi, im Shahzaib"
+print(message) #print the 1st message
+
+#change the value of the variable to a new message
+message = "I love playing Cricket"
+print(message) #printing the new one
