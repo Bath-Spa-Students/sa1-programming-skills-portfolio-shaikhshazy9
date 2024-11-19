2c5216a26cab51148c824acf4e8454eeb49d5105
shaikhshazy9 <shaikhshazy789@gmail.com>
2024-11-19 12:55:47 +0400

    Variables (fav no.)

--- Bonus exercises/Variables fav no.py
+++
@@ -1,7 +0,0 @@
-"""Q- Use a variable to represent your favorite number. Then, using that variable, create a
-message that reveals your favorite number. Print that message."""
-#use a variable to represent your favorite number
-number = 56
-#create a message that shows your fav no.
-message = "My favorite number is " + str(number)
-print(message) #print the final message 
