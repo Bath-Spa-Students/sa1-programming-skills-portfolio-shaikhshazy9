2edcb40121bef9f83c965ce2179f71b054238113
shaikhshazy9 <shaikhshazy789@gmail.com>
2024-11-19 23:50:57 +0400

    List (names)

---
+++ z_Bonus exercises/List names.py
@@ -0,0 +1,11 @@
+"""Q- Store the names of a few of your friends in a list called names. Print each person’s name
+by accessing each element in the list, one at a time."""
+#This is the list of names
+names = ["Abdullah", "Rehan", "Mansoor", "Arslan" , "Muhammad"]
+#now print each of the name using index
+print("Printing names using index: ")
+print(names[0]) #prints first name
+print(names[1]) #prints second name
+print(names[2]) #prints third name 
+print(names[3]) #prints fourth name
+print(names[4]) #prints the fifth name
