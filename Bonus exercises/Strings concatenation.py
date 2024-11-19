ecdf28ff538527926f878bffee7d550162ac0475
shaikhshazy9 <shaikhshazy789@gmail.com>
2024-11-19 12:54:17 +0400

    String concantenation

--- Bonus exercises/Strings concatenation.py
+++
@@ -1,8 +0,0 @@
-"""Q: Write three strings in different variables and print the output as one string."""
-#Give values to all three variables
-first = "Im"
-second = "Shahzaib"
-third = "Shaikh"
-#combine all the strings using + 
-all = first + " " + second + " " + third
-print(all) #now print the final result
